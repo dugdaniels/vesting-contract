5a38a148c794f1d7bd32773ed3d4008e958bcf42
DugDaniels <cdouglasd@gmail.com>
2021-10-16 14:51:08 -0400

    Build out contract

--- contracts/Entitlement.sol
+++ contracts/Entitlement.sol
@@ -4,41 +4,70 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-
 contract Entitlement is Ownable {
-
+    
     using SafeERC20 for IERC20;
+
+    enum Status {
+        Pending,
+        Active,
+        Completed
+    }
 
     address depositor;
     address public recipient;
-    address token;
-    uint originationTime = block.timestamp;
-    uint releaseTime;
+    address public token;
+    uint public amount;
+    uint public periodInDays;
+    uint public startTime;
+    Status public status;
 
-    constructor(address _recipient, address _token, uint _periodInDays) {
+    constructor(address _recipient, address _token, uint _amount, uint _periodInDays) {
+        require(_amount > 0, "Token amount must be greater than zero");
         recipient = _recipient;
         token = _token;
-        releaseTime = block.timestamp + (_periodInDays * 1 days);
+        amount = _amount;
+        periodInDays = _periodInDays;
     }
 
-    function fundEntitlement(uint _amount) public onlyOwner {
-        IERC20(token).safeTransferFrom(msg.sender, address(this), _amount);
+    modifier onlyActive() {
+        require(status == Status.Active, "Entitlement is not active");
+        _;
     }
 
-    function terminate() public onlyOwner {
-
+    function getReleaseTime() public view returns (uint) {
+        return startTime + (periodInDays * 1 days);
     }
 
-    function accelerate() public onlyOwner {
-
+    function fundEntitlement() public onlyOwner {
+        require(status == Status.Pending, "Entitlement has already been funded");
+        status = Status.Active;
+        startTime = block.timestamp;
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
     }
 
-    // function claim() public {
-    //     require(msg.sender == recipient, "Only the recipeient can claim funds");
-    //     require(block.timestamp >= releaseTime, "The vestement period has not passed");
-    //     uint payout = balance;
-    //     balance = 0;
-    //     IERC20(token).safeTransferFrom(address(this), recipient, payout);
-    // }
+    function accelerate() public onlyOwner onlyActive {
+        periodInDays = 0;
+    }
+
+    function terminate() public onlyOwner onlyActive {
+        status = Status.Completed;
+        if (block.timestamp >= getReleaseTime()) {
+            IERC20(token).safeTransfer(recipient, amount);
+        } else {
+            uint bigNum = 1_000_000_000_000_000_000;
+            uint percentEarned = (block.timestamp - startTime) / 1 days * bigNum / periodInDays;
+            uint amountEarned = percentEarned * amount / bigNum;
+            IERC20(token).safeTransfer(recipient, amountEarned);
+            IERC20(token).safeTransfer(owner(), amount - amountEarned);
+        }
+    }
+
+    function claim() public onlyActive {
+        require(msg.sender == recipient, "Only the recipeient can claim funds");
+        require(block.timestamp >= getReleaseTime(), "Vestement period has not passed");
+        status = Status.Completed;
+        IERC20(token).safeTransfer(recipient, amount);
+    }
 
 }
--- test/Entitlement.js
+++ test/Entitlement.js
@@ -4,20 +4,24 @@ const { ethers } = require("hardhat");
 describe("Entitlement contract", () => {
 
   let testToken;
+  let tokenSupply = 1_000_000;
   let entitlement;
   let owner;  
   let recipient;
+  const amount = 100;
+  const period = 90;
+  const secondsPerDay = 86_400;
 
   beforeEach(async () => {
     const TestToken = await ethers.getContractFactory("TestToken");
-    testToken = await TestToken.deploy("Test Token", "T_T", 1_000_000);
+    testToken = await TestToken.deploy("Test Token", "T_T", tokenSupply);
 
     [owner, recipient] = await ethers.getSigners();
 
     const Entitlement = await ethers.getContractFactory("Entitlement");
-    entitlement = await Entitlement.deploy(recipient.address, testToken.address, 30);
+    entitlement = await Entitlement.deploy(recipient.address, testToken.address, amount, period);
 
-    testToken.increaseAllowance(entitlement.address, 100);
+    testToken.increaseAllowance(entitlement.address, amount);
   });
 
   describe("Deployment", () => {
@@ -25,9 +29,127 @@ describe("Entitlement contract", () => {
       expect(await entitlement.owner()).to.equal(owner.address);
     });
 
-    it("Allows the owner to fund the entitlement", async () => {
-      await entitlement.fundEntitlement(100);
-      expect(await testToken.balanceOf(entitlement.address)).to.equal(100);
+    it("Sets the entitlement details correctly", async () => {
+      expect(await entitlement.recipient()).to.equal(recipient.address);
+      expect(await entitlement.token()).to.equal(testToken.address);
+      expect(await entitlement.periodInDays()).to.equal(period);
+    });
+
+    it("It is created as pending", async () => {
+      expect(await entitlement.status()).to.equal(0);
+    });
+  });
+
+  describe("Funding", () => {
+    it("Should allow the owner to fund the entitlement", async () => {
+      await entitlement.fundEntitlement();
+      expect(await testToken.balanceOf(entitlement.address)).to.equal(amount);
+    });
+
+    it("Should not allow non-owners to fund the entitlement", async () => {
+      await expect(
+        entitlement.connect(recipient).fundEntitlement()
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("Should only allow funding once", async () => {
+      await entitlement.fundEntitlement();
+      await expect(
+        entitlement.fundEntitlement()
+      ).to.be.revertedWith("Entitlement has already been funded");
+    });
+
+    it("Should set entitlement status to active when funded", async () => {
+      await entitlement.fundEntitlement();
+      expect(await entitlement.status()).to.equal(1);
+    });    
+    
+    it("Should set the start time to the current block timestamp", async () => {
+      await entitlement.fundEntitlement();
+
+      const blockNumber = await ethers.provider.getBlockNumber();
+      const block = await ethers.provider.getBlock(blockNumber);
+      const timestamp = block.timestamp;
+      expect(await entitlement.startTime()).to.equal(timestamp);
+    });
+
+    it("Should calculate a correct release time", async () => {
+      await entitlement.fundEntitlement();
+
+      const blockNumber = await ethers.provider.getBlockNumber();
+      const block = await ethers.provider.getBlock(blockNumber);
+      const timestamp = block.timestamp;
+      const expectedReleaseTime = period * secondsPerDay + timestamp
+
+      expect(await entitlement.getReleaseTime()).to.equal(expectedReleaseTime);
+    });
+  });  
+
+  describe("Claiming entitlement", () => {
+    it("Should transfer token balance to the recipient", async () => {
+      await entitlement.fundEntitlement();
+      await network.provider.send("evm_increaseTime", [period * secondsPerDay]);
+      await entitlement.connect(recipient).claim();
+      expect(await testToken.balanceOf(recipient.address)).to.equal(amount);
+      expect(await testToken.balanceOf(entitlement.address)).to.equal(0);
+    });
+
+    it("Should only allow claiming after vesting", async () => {
+      await entitlement.fundEntitlement();
+      await expect(
+        entitlement.connect(recipient).claim()
+      ).to.be.revertedWith("Vestement period has not passed");
+
+      await network.provider.send("evm_increaseTime", [period * secondsPerDay]);
+      await entitlement.connect(recipient).claim();
+      expect(await testToken.balanceOf(recipient.address)).to.equal(amount);
+      expect(await testToken.balanceOf(entitlement.address)).to.equal(0);
+    });
+
+    it("Should only allow the recipient to claim the entitlement", async () => {
+      await entitlement.fundEntitlement();
+      await expect(
+        entitlement.claim()
+      ).to.be.revertedWith("Only the recipeient can claim funds");
+    });
+
+    it("Should set entitlement status to completed when claimed", async () => {
+      await entitlement.fundEntitlement();
+      await network.provider.send("evm_increaseTime", [period * secondsPerDay]);
+      await entitlement.connect(recipient).claim();
+      expect(await entitlement.status()).to.equal(2);
+    }); 
+  });
+
+  describe("Terminating entitlement", () => {
+    it("Should split token balances if not fully vested", async () => {
+      await entitlement.fundEntitlement();
+      await network.provider.send("evm_increaseTime", [(period / 5) * secondsPerDay]);
+      await entitlement.terminate();
+      expect(await testToken.balanceOf(recipient.address)).to.equal(amount / 5);
+      expect(await testToken.balanceOf(owner.address)).to.equal(tokenSupply - amount / 5);
+    });
+    
+    it("Should give recipent full balance if vested", async () => {
+      await entitlement.fundEntitlement();
+      await network.provider.send("evm_increaseTime", [period * secondsPerDay]);
+      await entitlement.terminate();
+      expect(await testToken.balanceOf(recipient.address)).to.equal(amount);
+    });
+  });
+
+  describe("Accelerating payout", () => {
+    it("Should allow the owner can accelerate the entitlement", async () => {
+      await entitlement.fundEntitlement();
+      await entitlement.accelerate();
+      expect(await entitlement.periodInDays()).to.equal(0);
+    });
+
+    it("Should not allow non-owners to accelerate the entitlement", async () => {
+      await entitlement.fundEntitlement();
+      await expect(
+        entitlement.connect(recipient).accelerate()
+      ).to.be.revertedWith("Ownable: caller is not the owner");
     });
   });
 
